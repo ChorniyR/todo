4892f9e15858c67812b988722be351e4ceff6447
ChorniyR <pomak803@gmail.com>
2021-08-11 16:32:34 +0300

    add viewset for Test model

--- todo/core/models.py
+++ todo/core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Dayset(models.Model):
+    date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.date}({self.user})"
+
+
+class Task(models.Model):
+    class State(models.TextChoices):
+        ACTIVE = 'a', ('Active')
+        COMPLETED = 'c', ('Completed')
+
+    title = models.CharField(max_length=256) 
+    details = models.TextField()
+    creation_date = models.DateTimeField(auto_now=True)
+    state = models.CharField(max_length=1, choices=State.choices, default=State.ACTIVE)
+    dayset = models.ForeignKey(Dayset, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- todo/core/views.py
+++ todo/core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import TaskSerializer
+from .models import Task
+from .permissions import IsOwner
+
+
+class TestViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permissions = [IsAuthenticated, IsOwner]
